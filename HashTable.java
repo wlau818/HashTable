a77f6d958f49dd7ab5c769485fc6a616f099d75f
wlau818 <31222275+wlau818@users.noreply.github.com>
2017-12-11 21:50:01 -1000

    Add files via upload

---
+++ HashTable.java
@@ -0,0 +1,119 @@
+/*
+ * This class sets up the has table data structure along with
+ * the utility methods to manipulate it.
+ */
+public class HashTable {
+
+	private Node[] map;
+	private int tableSize;
+	
+	// constructor
+	public HashTable(int tableSize) {
+		map = new Node[tableSize];
+		this.tableSize = tableSize;
+	}
+	
+	/*
+	 * This is the hash function.
+	 * For this hash function, return 0 if key is the empty string.
+	 * Otherwise, calculate the sum of the ASCII values of the characters in key
+	 * and find the remainder when divided by 8 (remember mod divisision %).
+	 * Check the java API for String methods that may be helpful.
+	 */
+	private int hash(String key) {
+		if (key == null) {
+			return 0;
+		} else {
+			String[] code = key.split("");
+			int num = 0;
+			for (int i = 0; i < code.length; i ++) {
+				num = code[i].hashCode() + num;
+			}
+			num = num % 8;
+			return num;
+		}
+		// your code here
+	}
+	
+	/*
+	 * This method should insert a node containing the given key
+	 * in the proper bucket in the hash table.  Insert new nodes
+	 * at the head of each linked list for ease of implementation.
+	 */
+	public void insert(String key) {
+		Node n = new Node(key);
+		if (map[hash(key)].next != null) {
+			Node temp = new Node(map[hash(key)].record);
+			map[hash(key)].record = key;
+			map[hash(key)].next = temp;
+		} else {
+			map[hash(key)] = n;
+		}
+		// your code here
+	}
+	
+	/*
+	 * This method returs true if the search key is contained in the
+	 * hash table, and false otherwise.
+	 */
+	public boolean search(String key) {
+		int num = hash(key);
+		Node pointer = map[num];
+		int i = 0;
+		while (pointer != null && i == 0) {
+			if (pointer.record == map[num].record) {
+				i ++;
+			} else {
+				pointer = pointer.next;
+			}
+		}
+		if (i > 0) {
+			return true;
+		} else {
+			return false;
+		}
+		// your code here
+	}
+	
+	/*
+	 * This method should print out the hash table row by row.
+	 * Each line should print out a separate row of the table.
+	 * Print the index followed a colon and then the records in that
+	 * bucket with spaces between them.
+	 * 
+	 * For example:
+	 * 1: Nicholas Mary Kim Jack
+	 */
+	public void printTable() {
+		Node pointer = map[0];
+		for (int i = map.length; i < 0; i ++) {
+			while (pointer != null) {
+				System.out.println(pointer.record);
+				pointer = pointer.next;
+			}
+			pointer = map[i];
+			
+		}
+		// your code here
+	}
+	
+	/*
+	 * This method should delete ALL nodes matching the search key.
+	 */
+	public void delete(String key) {
+		int num = hash(key);
+		Node pointer = map[num];
+		int i = 0;
+		while (pointer != null && i == 0) {
+			if (pointer.next.record.equals(key)) {
+				pointer.next = null;
+				i ++;
+			} else {
+				pointer = pointer.next;
+			}
+		
+		}
+		// your code here
+	}
+	
+}
---
+++ Node.java
@@ -0,0 +1,16 @@
+/*
+ * This class sets up the basic node used in the has table.
+ * Do not change this class.
+ */
+public class Node {
+
+	public String record = "";
+	public Node next = null;
+	
+	// constructor
+	public Node(String newRecord) {
+		record = newRecord;
+		next = null;
+	}
+	
+}
